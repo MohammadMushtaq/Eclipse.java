9799c00688d7ed6a5f52efda571baa663526f432
Mohammad Mushtaq <91558944+MohammadMushtaq@users.noreply.github.com>
2021-12-30 11:33:24 +0530

    Delete FestivalConstant.java

--- JDBC/src/com/xworkz/jdbc/DBconstants/FestivalConstant.java
+++
@@ -1,9 +0,0 @@
-package com.xworkz.jdbc.DBconstants;
-
-public interface FestivalConstant {
-
-	String MY_URL="jdbc:mysql://localhost:3306/festival";
-	String USERNAME="root";
-	String PASSWORD ="root";
-	String DRIVER="com.mysql.cj.jdbc.Driver";
-}
