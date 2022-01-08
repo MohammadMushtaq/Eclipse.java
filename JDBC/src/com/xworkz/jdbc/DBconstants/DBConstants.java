3afc82a7e7d34616a7fd510d1f11290d7bf2fcfc
unknown <mushtaqmech.rymec@gmail.com>
2022-01-08 11:43:17 +0530

    adding hiber

--- JDBC/src/com/xworkz/jdbc/DBconstants/DBConstants.java
+++
@@ -1,8 +0,0 @@
-package com.xworkz.jdbc.DBconstants;
-
-public interface DBConstants {
-	String MY_URL="jdbc:mysql://localhost:3306/btm";
-	String USERNAME="root";
-	String PASSWORD ="root";
-	String DRIVER="com.mysql.cj.jdbc.Driver";
-}
